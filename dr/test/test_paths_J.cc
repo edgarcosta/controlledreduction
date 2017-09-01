db82ed6398808942e57e7412d4c82531ac036da3
Edgar Costa <edgarcosta@math.dartmouth.edu>
2017-09-01 14:42:10 -0400

    dr tests

---
+++ dr/test/test_paths_J.cc
@@ -0,0 +1,50 @@
+// Copyright 2013-2017 Edgar Costa
+// See LICENSE file for license details.
+//
+// runs test_paths_J for some random curves of degree 3,4 and 5 and K3 surfaces
+
+   
+#include "dr.h"
+#include "timing.h"
+
+#include <iostream>
+
+#define NTL_RANGE_CHECK
+int main() {
+    timestamp_pair pair;
+    timestamp_mark(pair);
+    SetSeed(to_ZZ(time(NULL)));
+    cout<<"test_paths_J...";
+    fflush(stdout);
+    for (int64_t i = 0; i < 10; ++i) {
+        int64_t p = 0;
+        while(p < 43)
+            p = GenPrime_long(13);
+        int64_t precision = 4;
+        for (int64_t n = 2; n < 4; ++n) {
+            int64_t degree_bound = 5;
+            int64_t N = 5;
+            if (n == 3) {
+                degree_bound = 4;
+                N = 3;
+            }
+            for (int64_t d = n + 1; d <= degree_bound; d++) {
+                zz_p::init(p);
+                ZZ_p::init( power_ZZ(p, precision) );
+                de_Rham_local D;
+                //cout << p <<" "<<n<<" "<<d<<endl; 
+                D = de_Rham_local(p, precision, n, d, false);
+                bool test_success = D.test_paths_J(N, N);;
+                if (not test_success ) {
+                    cout << "\n\tFAIL test_monomial_to_basis_J(2)" <<endl;
+                    cout << "\tp = " << p << " n = " << n << " d = " << d << " N = " << N << endl;
+                    abort();
+                }
+            }
+        }
+    }
+    cout << "PASS ";
+    timestamp_report(pair);
+    cout << endl;
+    return 0;
+}
