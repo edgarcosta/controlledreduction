d60ec210a84dfd30a6502716124f4ce6ca9a815d
Edgar Costa <edgarcosta@nyu.edu>
2017-09-29 20:59:52 -0600

    spaces

--- hypersurface.h
+++ hypersurface.h
@@ -35,19 +35,39 @@ class hypersurface{
         hypersurface() {};
         virtual ~hypersurface(){};
         
-        hypersurface(int64_t p, int64_t precision, int64_t n, int64_t d,Vec<zz_p> fbar_vector, bool verbose);
+        hypersurface(int64_t p, int64_t precision, int64_t n, int64_t d, Vec<zz_p> fbar_vector, bool verbose);
         hypersurface(int64_t p, int64_t precision, int64_t n, int64_t d, bool verbose);
         hypersurface(int64_t p, int64_t precision, map< Vec<int64_t>, zz_p, vi64less> fbar, bool verbose);
         hypersurface(const char * filename);
         virtual bool save(const char * filename);
 
-                
+        /*
+         * assures the array fpow 
+         * (where we store the powers of f)
+         * has enough powers of f
+         */ 
         void compute_fpow(int64_t power);
 
 
-        Vec<ZZ_p> frob_J(const int64_t coordinate, const int64_t N);
-        Vec<ZZ_p> frob_J_ZZ(const int64_t coordinate, const int64_t N, const int64_t loop);
+
+        /*
+         * returns the coordinates of the p-adic approximation of frob(e_i)
+         * using N terms
+         */
+        Vec<ZZ_p> frob_J(const int64_t coordinate, const int64_t N)
+        {
+            // ZZ_p is broken! FIXME
+            return frob_J_ZZ(coordinate, N, 0);
+        };
+        // working over ZZ_p
         Vec<ZZ_p> frob_J_ZZ_p(const int64_t coordinate, const int64_t N, const int64_t loop);
+        // using reduce_vector_J_poly_ZZ
+        Vec<ZZ_p> frob_J_ZZ(const int64_t coordinate, const int64_t N, const int64_t loop);
+
+        /*
+         * returns the matrix coordinates of the p-adic approximation of frob(e_i)
+         * using N_k terms, where (k+1) is the pole order of e_i
+         */
         Mat<ZZ_p> frob_matrix_J(Vec<int64_t> N);
 
 };
