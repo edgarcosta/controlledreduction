864eb5ce7e3d595668dad476a5820085034e0017
Edgar Costa <edgarcosta@math.dartmouth.edu>
2018-09-27 12:51:39 -0400

    some context

--- examples/zeta_funtion_wrapper_K3.cc
+++ examples/zeta_funtion_wrapper_K3.cc
@@ -12,6 +12,11 @@ using namespace NTL;
 int main()
 {
 
+  //format:
+  // prime                  p = 23
+  // monomials              x^4 x*y^3 z*w^3 x^3*y z^3*w
+  // monomial coefficients  1 1 1 1
+  // --> x^4 +  x*y^3 + z*w^3 + x^3*y + z^3*w = 0 over F_23
   char buffer[] = "23\n[[4 0 0 0] [1  3  0  0]  [0  0  1  3]  [3  1  0  0]  [0  0  3  1]]\n[1 1  1  1  1]";
   ZZX cp;
   zeta_function(cp, buffer, true, 1);
